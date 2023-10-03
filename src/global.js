fe63fa63ade6174cfbcd4e8061e0e20f8548960a
harikakshinthala <harikaakshinthala1@gmail.com>
2023-10-03 16:58:14 +0530

    renderlink

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
 //for cnnection to backend
-export const API = " http://localhost:4000"
+//export const API = " http://localhost:4000"
 
-// export const API = " https://bookapp-backend2.onrender.com"
+ export const API = " https://inventoryapp-wxpx.onrender.com"
